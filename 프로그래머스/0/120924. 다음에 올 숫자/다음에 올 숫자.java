a4547e48eb970adea11bb6b964b552edacfa0875
jimnyy <167507672+jimnyy@users.noreply.github.com>
2024-06-29 15:25:55 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,12 @@
+class Solution {
+     public int solution(int[] common) {
+        int answer = 0;
+        int i = 0;
+        if (common[i + 1] - common[i] == common[i + 2] - common[i + 1]) {
+            answer = common[common.length-1] + (common[i + 1] - common[i]);
+        } else { answer = common[common.length-1] * (common[1] / common[0]);
+
+        }
+        return answer;
+    }
+}
