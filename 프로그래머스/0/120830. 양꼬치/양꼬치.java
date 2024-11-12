88f35e46466ad2fee9f5e4afee8df9e4bd9bc973
jimnyy <167507672+jimnyy@users.noreply.github.com>
2024-11-12 14:32:46 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = n*(12000) + k*(2000);
+        if(n>9){
+            answer -=(n/10) * 2000;
+        }
+        return answer;
+    }
+}
